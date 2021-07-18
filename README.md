4d0ff9f1a1baf1d85e38edf5c90cd723105861c2
emortal <nathanpenwill88@gmail.com>
2021-07-18 22:33:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# ExampleExtension
+An example extension for Minestom based off of Sabre/kotlin libs.
+
+## Installation
+
+Download the jar from [Releases](https://github.com/Project-Cepi/ExampleExtension/releases)
+OR compile it yourself. Instructions to do so are in Compile header
+
+Drop it into the `/extensions` folder.
+
+## Compile
+
+Create a folder, then
+Clone the repository using:
+
+`git clone https://github.com/Project-Cepi/Sabre.git`
+
+Once it is cloned, make sure you have gradle installed, and run
+
+`./gradlew shadowJar` on Mac or Linux, and
+
+`gradlew shadowJar` on Windows.
+
+This will output the jar to `build/libs` in the project directory.
---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.5.20"
+    // Kotlinx serialization for any data format
+    kotlin("plugin.serialization") version "1.4.21"
+    // Shade the plugin
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    // Allow publishing
+    `maven-publish`
+
+    // Apply the application plugin to add support for building a jar
+    java
+    // Dokka documentation w/ kotlin
+    id("org.jetbrains.dokka") version "1.5.0"
+}
+
+repositories {
+    // Use mavenCentral
+    mavenCentral()
+
+    maven(url = "https://jitpack.io")
+    maven(url = "https://repo.spongepowered.org/maven")
+    maven(url = "https://repo.minestom.com/repository/maven-public/")
+    maven(url = "https://repo.velocitypowered.com/snapshots/")
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    compileOnly(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    compileOnly(kotlin("stdlib"))
+
+    // Use the Kotlin reflect library.
+    compileOnly(kotlin("reflect"))
+
+    // Compile Minestom into project
+    compileOnly("com.github.Minestom:Minestom:-SNAPSHOT")
+
+    // import kotlinx serialization
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+configurations {
+    testImplementation {
+        extendsFrom(configurations.compileOnly.get())
+    }
+}
+
+// Take gradle.properties and apply it to resources.
+tasks {
+    processResources {
+        // Apply properties to extension.json
+        filesMatching("extension.json") {
+            expand(project.properties)
+        }
+    }
+
+    // Set name, minimize, and merge service files
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        archiveBaseName.set(project.name)
+        mergeServiceFiles()
+        minimize()
+    }
+
+    test { useJUnitPlatform() }
+
+    // Make build depend on shadowJar as shading dependencies will most likely be required.
+    build { dependsOn(shadowJar) }
+
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-Xinline-classes")
+}
