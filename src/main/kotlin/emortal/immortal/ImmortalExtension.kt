2c1b873baea0c16884d5861307f07cc533127c91
emortal <nathanpenwill88@gmail.com>
2021-08-21 18:42:23 +0100

    Handle disconnecting/changing world properly

--- src/main/kotlin/emortal/immortal/ImmortalExtension.kt
+++ src/main/kotlin/emortal/immortal/ImmortalExtension.kt
@@ -1,14 +1,29 @@
 package emortal.immortal
 
+import emortal.immortal.game.GameManager.game
+import net.minestom.server.event.player.PlayerDisconnectEvent
+import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.extensions.Extension
 import world.cepi.kstom.command.register
+import world.cepi.kstom.event.listenOnly
 
 class ImmortalExtension : Extension() {
 
     override fun initialize() {
-        logger.info("[ImmortalExtension] has been enabled!")
+        eventNode.listenOnly<PlayerDisconnectEvent> {
+            player.game?.removePlayer(player)
+        }
+        eventNode.listenOnly<PlayerSpawnEvent> {
+            if (player.game != null) {
+                if (player.game!!.instance != spawnInstance) {
+                    player.game?.removePlayer(player)
+                }
+            }
+        }
 
         PlayCommand.register()
+
+        logger.info("[ImmortalExtension] has been enabled!")
     }
 
     override fun terminate() {
