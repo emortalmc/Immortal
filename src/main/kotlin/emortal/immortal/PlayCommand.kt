e32dbbe33eb63da9b90ffcd3cb6b28ee05481f57
emortal <nathanpenwill88@gmail.com>
2021-08-21 18:47:40 +0100

    Few changesAdd fade effect to /play command and remove player from previous game before adding to new game

--- src/main/kotlin/emortal/immortal/PlayCommand.kt
+++ src/main/kotlin/emortal/immortal/PlayCommand.kt
@@ -1,14 +1,18 @@
 package emortal.immortal
 
 import emortal.immortal.game.GameManager
+import emortal.immortal.game.GameManager.game
 import emortal.immortal.game.GameManager.joinGameOrNew
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.title.Title
 import net.minestom.server.command.builder.Command
 import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException
+import world.cepi.kstom.Manager
 import world.cepi.kstom.command.addSyntax
 import world.cepi.kstom.command.arguments.suggest
+import java.time.Duration
 
 object PlayCommand : Command("play") {
 
@@ -26,8 +30,32 @@ object PlayCommand : Command("play") {
         addSyntax(gamemodearg) {
             val gamemode = context.get(gamemodearg)
 
-            sender.asPlayer().joinGameOrNew(gamemode.key, GameManager.registeredGameMap[gamemode.key]!!.defaultGameOptions)
-            sender.asPlayer().sendMessage(Component.text("Joining ${gamemode.value.gameName}...", NamedTextColor.GREEN))
+            if (!sender.isPlayer) return@addSyntax
+
+            val player = sender.asPlayer()
+
+            if (player.game != null) {
+                player.game!!.playerLeave(player)
+            }
+
+            player.sendActionBar(Component.text("Joining ${gamemode.value.gameName}...", NamedTextColor.GREEN))
+
+            player.showTitle(
+                Title.title(
+                    Component.text("\uE00A"),
+                    Component.empty(),
+                    Title.Times.of(
+                        Duration.ofMillis(500),
+                        Duration.ofMillis(250),
+                        Duration.ofMillis(500)
+                    )
+                )
+            )
+
+            Manager.scheduler.buildTask {
+                sender.asPlayer().joinGameOrNew(gamemode.key, GameManager.registeredGameMap[gamemode.key]!!.defaultGameOptions)
+            }.delay(Duration.ofMillis(500)).schedule()
+
         }
     }
 
