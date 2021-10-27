b8c7a5acc683811e26dde98ad520f8d618e70703
emortal <nathanpenwill88@gmail.com>
2021-10-27 16:13:08 +0100

    1.1.0

--- src/main/kotlin/dev/emortal/immortal/ImmortalExtension.kt
+++ src/main/kotlin/dev/emortal/immortal/ImmortalExtension.kt
@@ -5,7 +5,6 @@ import dev.emortal.immortal.game.GameManager.game
 import net.minestom.server.event.player.PlayerDisconnectEvent
 import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.extensions.Extension
-import world.cepi.kstom.command.register
 import world.cepi.kstom.event.listenOnly
 
 class ImmortalExtension : Extension() {
