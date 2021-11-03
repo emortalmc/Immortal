872726deb62ede0dc6977f8a4453d589a252fea5
emortal <nathanpenwill88@gmail.com>
2021-11-03 23:57:39 +0000

    Remove title from kill in pvpgame

--- src/main/kotlin/dev/emortal/immortal/game/PvpGame.kt
+++ src/main/kotlin/dev/emortal/immortal/game/PvpGame.kt
@@ -1,32 +1,17 @@
 package dev.emortal.immortal.game
 
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextDecoration
-import net.kyori.adventure.title.Title
 import net.minestom.server.entity.Entity
 import net.minestom.server.entity.GameMode
 import net.minestom.server.entity.Player
-import java.time.Duration
 
 abstract class PvpGame(gameOptions: GameOptions) : Game(gameOptions) {
 
     // TODO: Make messages configurable
     fun kill(player: Player, killer: Entity? = null) {
-
-        val subtitle = Component.empty()
-
         player.clearEffects()
         player.heal()
         player.isInvisible = true
         player.gameMode = GameMode.SPECTATOR
-        player.showTitle(
-            Title.title(
-                Component.text("YOU DIED", NamedTextColor.RED, TextDecoration.BOLD),
-                subtitle,
-                Title.Times.of(Duration.ZERO, Duration.ofSeconds(2), Duration.ofSeconds(1))
-            )
-        )
         // TODO: global message when messages are configurable
 
         playerDied(player, killer)
