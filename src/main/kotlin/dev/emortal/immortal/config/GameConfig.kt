7058dbc40154c8857fc463c73f027f80e31c3c96
emortal <nathanpenwill88@gmail.com>
2022-03-26 19:07:15 +0000

    Make redis address configurable

--- src/main/kotlin/dev/emortal/immortal/config/GameConfig.kt
+++ src/main/kotlin/dev/emortal/immortal/config/GameConfig.kt
@@ -3,5 +3,6 @@ package dev.emortal.immortal.config
 @kotlinx.serialization.Serializable
 data class GameConfig(
     val serverName: String,
-    val serverPort: Int
+    val serverPort: Int,
+    val address: String = "redis://172.17.0.1:6379"
 )
--- src/main/kotlin/dev/emortal/immortal/util/RedisStorage.kt
+++ src/main/kotlin/dev/emortal/immortal/util/RedisStorage.kt
@@ -1,11 +1,12 @@
 package dev.emortal.immortal.util
 
+import dev.emortal.immortal.ImmortalExtension
 import org.redisson.Redisson
 import org.redisson.config.Config
 
 
 object RedisStorage {
 
-    val redisson = Redisson.create(Config().also { it.useSingleServer().setAddress("redis://localhost:6379").setClientName("Proxy") })
+    val redisson = Redisson.create(Config().also { it.useSingleServer().setAddress(ImmortalExtension.gameConfig.address).setClientName(ImmortalExtension.gameConfig.serverName) })
 
 }
