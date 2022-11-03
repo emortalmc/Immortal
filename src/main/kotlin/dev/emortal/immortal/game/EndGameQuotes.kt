9dd252969267fcbd3ab8f450045fee9949dcc350
emortal <me@emortal.dev>
2022-11-03 21:57:43 +0000

    many more quote

--- src/main/kotlin/dev/emortal/immortal/game/EndGameQuotes.kt
+++ src/main/kotlin/dev/emortal/immortal/game/EndGameQuotes.kt
@@ -1,29 +1,50 @@
 package dev.emortal.immortal.game
 
+import net.minestom.server.entity.Player
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
+
 object EndGameQuotes {
 
-    val victory = setOf(
+    private val victory = setOf(
         "pepeD",
         "gg ez no re",
         "All luck",
         "NoHacksJustSkill",
         "Were you hacking?",
         "Were you teaming?",
-        "skillage",
+        "no skill issues here",
+        "i am better",
         "I dropped my popcorn",
         "Defeat... but not for you",
         "Teamwork makes the dream work",
         "vic roy",
         "call an ambulance... but not for me",
         "What chair do you have?",
+        "I won? It must have been my chair!",
         "No GC this time",
-        "Moulberry is proud of you"
+        "Moulberry is proud of you",
+        "tryhard == true",
+        "touch grass",
+        "take a shower",
+        "take a cookie for your efforts",
+        "[username] never dies!",
+        "average rust enjoyer",
+        "i wasn't looking, do it again",
+        "built different",
+        "carried",
+        "want a medal?",
+        "you lost the game"
     )
 
-    val defeat = setOf(
+    private val defeat = setOf(
         "I guess you weren't trying",
         "no",
         "L",
+        "rip bozo",
+        "no zaza??",
+        "what is your excuse?",
+        "skill issue",
         "uffeyman was here",
         "Better luck next time!",
         "Worse luck next time!",
@@ -37,17 +58,20 @@ object EndGameQuotes {
         "PepeHands",
         "At least someone won, too bad it wasn't you",
         "cry about it",
+        "were you afk?",
         "smh my head",
         "I bet you didn't even say good game",
         "You did your best... but it wasn't enough",
         "\"i lagged\" said an Australian",
         "If I had a dollar every time you won a game, I'd be in crippling debt",
         "widepeepoSad",
+        "try harder",
         "You clearly weren't using Graphite",
-        "I saw the GC spike"
+        "I saw the GC spike",
+        "python user"
     )
 
-    val draw = setOf(
+    private val draw = setOf(
         "Well that wasn't worth my time",
         "Hopefully one of you win next time",
         "Well... that was unexpected",
@@ -55,7 +79,23 @@ object EndGameQuotes {
         "Feeling a bit indecisive?",
         "It would have been better if someone won",
         "You're both average",
-        "You both suck!"
+        "You both suck!",
+        "Twins?"
     )
 
+    fun getVictoryQuote(player: Player): String {
+        val quote = victory.random().replace("[username]", player.username)
+
+        if (quote == "take a cookie for your efforts") {
+            player.itemInMainHand = ItemStack.of(Material.COOKIE)
+        }
+
+        return quote
+    }
+    fun getDefeatQuote(player: Player): String {
+        return defeat.random().replace("[username]", player.username)
+    }
+    fun getDrawQuote(player: Player): String {
+        return draw.random().replace("[username]", player.username)
+    }
 }
